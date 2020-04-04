73df9b792ded33053d242d13fb3f59c9e509169b
caribbeanyute <smithcleon@gmail.com>
2020-04-04 04:35:54 +0000

    Modified Procfile

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,8 @@ from os.path import join, dirname, realpath
 app = Flask(__name__)
 #app.config['SECRET_KEY'] = "change this to be a more random key"
 app.config['SECRET_KEY'] = secrets.token_urlsafe(32)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://proj:usersdatabase@localhost/users"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://sutkdnkjmzucrd:dd0f28e117aeaa510f57d36d180f78e20d10724b4754662f307faf132cbbe840@ec2-52-87-58-157.compute-1.amazonaws.com:5432/dcq5sp3v71dd68"
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 #Uploads Folder
